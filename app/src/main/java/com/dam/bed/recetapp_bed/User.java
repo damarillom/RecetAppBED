2d8e65f48364dabf560ca433f1fd8a97552466f9
damarillom <danielamarillo1@hotmail.com>
2019-04-25 20:06:59 +0200

    User actualizado con getters y setters

--- app/src/main/java/com/dam/bed/recetapp_bed/User.java
+++ app/src/main/java/com/dam/bed/recetapp_bed/User.java
@@ -1,8 +1,14 @@
 package com.dam.bed.recetapp_bed;
 
+import java.util.Date;
+
 public class User {
     public String emailId;
-    public double imc;
+    public double weight;
+    public double height;
+    public char gender;
+    public Date birthday;
+    public int diet;
 
     public User() {
 
@@ -10,13 +16,53 @@ public class User {
 
     public User(String emailId) {
         this.emailId = emailId;
-
-
     }
 
-    public User(String emailId, double imc) {
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setEmailId(String emailId) {
         this.emailId = emailId;
-        this.imc = imc;
     }
 
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setDiet(int diet) {
+        this.diet = diet;
+    }
+
+    public int getDiet() {
+        return diet;
+    }
 }
