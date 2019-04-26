fb3975786a16ec758cbeafcbd6fe9f43ad523a96
Blacostnero <borja.lacosta@gmail.com>
2019-04-26 17:24:53 +0200

    cambiar nombre de layout en Cuestionario

--- app/src/main/java/com/dam/bed/recetapp_bed/Cuestionario.java
+++ app/src/main/java/com/dam/bed/recetapp_bed/Cuestionario.java
@@ -8,6 +8,6 @@ public class Cuestionario extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.questions);
+        setContentView(R.layout.activity_cuestionario);
     }
 }
