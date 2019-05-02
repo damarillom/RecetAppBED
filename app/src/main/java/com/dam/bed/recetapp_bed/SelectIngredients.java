91d760100afd1375f25a670e9636bc8b6f4296f0
Blacostnero <borja.lacosta@gmail.com>
2019-05-02 20:47:53 +0200

    Se ve las dos listas. Lista de todos los ingredientes y lista de los ingredientes que el usuario excluye.
    Errores
    - Falla el añadir ingrediente cuando se hace una búsqueda

--- app/src/main/java/com/dam/bed/recetapp_bed/SelectIngredients.java
+++ app/src/main/java/com/dam/bed/recetapp_bed/SelectIngredients.java
@@ -1,14 +1,23 @@
 package com.dam.bed.recetapp_bed;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,9 +26,16 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
-public class SelectIngredients extends AppCompatActivity {
+import java.util.ArrayList;
 
-    TextView tIngredients;
+import static com.dam.bed.recetapp_bed.R.string.searchingredient;
+
+public class SelectIngredients extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    SearchView mSearchView;
+    ListView mListView, mListView2;
+    ArrayList<String> ingredients = new ArrayList<>();
+    ArrayList<String> ingredientsNo = new ArrayList<>();
     DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Ingredients");
 
     @Override
@@ -28,9 +44,11 @@ public class SelectIngredients extends AppCompatActivity {
         setContentView(R.layout.activity_select_ingredients);
 
         //View elements
-        tIngredients = findViewById(R.id.tIngredients);
+        mSearchView = findViewById(R.id.searchView);
+        mListView = findViewById(R.id.listView);
+        mListView2 = findViewById(R.id.listView2);
 
-        // Attach a listener to read the data at our posts reference
+        // Conseguir los ingredientes
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -38,6 +56,7 @@ public class SelectIngredients extends AppCompatActivity {
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     Ingredient ingredient = ds.getValue(Ingredient.class);
                     System.out.println("ingredient - " + ingredient);
+                    ingredients.add(ingredient.getName());
                 }
             }
 
@@ -46,6 +65,85 @@ public class SelectIngredients extends AppCompatActivity {
                 System.out.println("The read failed: " + databaseError.getCode());
             }
         });
+
+        // Añadir los ingredientes a la lista
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_expandable_list_item_1,
+                ingredients);
+        mListView.setAdapter(adapter);
+        mListView.setBackgroundColor(Color.parseColor("#99ff99"));
+        mListView.setTextFilterEnabled(true);
+
+        // Lista 2 - Ingredientes que se excluyen
+        final ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this,
+                android.R.layout.simple_expandable_list_item_1,
+                ingredientsNo);
+        mListView2.setAdapter(adapter2);
+        mListView2.setBackgroundColor(Color.parseColor("#ff5050"));
+
+        setupSearchView();
+
+        //Onclick de la lista principal
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object o = mListView.getItemAtPosition(position);
+                String item = (String) o;
+                Snackbar.make(view, "Adding ingredient: "+item, Snackbar.LENGTH_SHORT).show();
+
+                // Añadir a la segunda lista
+//                ingredientsNo.add(item);
+                adapter2.add(item);
+                adapter2.notifyDataSetChanged();
+
+                // Eliminar de la lista principal
+                adapter.remove(item);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        // Onclick segunda lista
+        mListView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object o = mListView2.getItemAtPosition(position);
+                String item = (String) o;
+                Snackbar.make(view, "Removing ingredient: "+item, Snackbar.LENGTH_SHORT).show();
+
+                // Añadir a la primera lista
+//                ingredientsNo.add(item);
+                adapter.add(item);
+                adapter.notifyDataSetChanged();
+
+                // Eliminar de la lista secundaria
+                adapter2.remove(item);
+                adapter2.notifyDataSetChanged();
+            }
+        });
     }
+
+    private void setupSearchView() {
+        mSearchView.setIconifiedByDefault(false);
+        mSearchView.setOnQueryTextListener(this);
+//        mSearchView.setSubmitButtonEnabled(true);
+        mSearchView.setQueryHint("Search ingredient");
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)) {
+            mListView.clearTextFilter();
+        } else {
+            mListView.setFilterText(newText);
+        }
+        return true;
+    }
+
 }
 
