07c8255bd69360db2b0cc46777c16d14a7e54029
damarillom <danielamarillo1@hotmail.com>
2019-05-07 17:22:47 +0200

    Storage funcional hardcodeada la imagen

--- app/src/main/java/com/dam/bed/recetapp_bed/RecipeView.java
+++ app/src/main/java/com/dam/bed/recetapp_bed/RecipeView.java
@@ -1,31 +1,62 @@
 package com.dam.bed.recetapp_bed;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class RecipeView extends AppCompatActivity {
 
     static String ingredientes = "";
     static TextView title, ingredients, description;
+    static ImageView image;
+    final long ONE_MEGABYTE = 1024 * 1024;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_view);
 
+        image = (ImageView) findViewById(R.id.imageView2);
         title = (TextView) findViewById(R.id.title);
         ingredients = (TextView) findViewById(R.id.ingredients);
         description = (TextView) findViewById(R.id.description);
         String name = "Pollo con arroz";
         title.setText(name);
         ingredientes = "Ingredientes:\n";
+
+        //PRUEBA STORAGE
+        // Create a storage reference from our app
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+
+        StorageReference imageRef = storageRef.child("quinoas.jpg");
+
+        imageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                image.setImageBitmap(bitmap);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
 
 
         ValueEventListener valueEventListener = new ValueEventListener() {
@@ -42,33 +73,6 @@ public class RecipeView extends AppCompatActivity {
                 ingredients.setText(ingredientes);
 
                 description.setText(recipe.getDescription());
-                /**if (user.isQuest()) {
-                    editTextHeight.setText(Double.toString(user.getAltura()));
-                    editTextWeight.setText(Double.toString(user.getPeso()));
-                    editTextYear.setText(Integer.toString(user.getBirthday()));
-                    String gender = user.getGender();
-                    String diet = user.getDiet();
-                    RadioButton radioM = (RadioButton) findViewById(R.id.radioMale);
-                    RadioButton radioF = (RadioButton) findViewById(R.id.radioFemale);
-                    RadioButton radioO = (RadioButton) findViewById(R.id.radioOther);
-                    RadioButton radioVegano = (RadioButton) findViewById(R.id.radioVegan);
-                    RadioButton radioVegetarian = (RadioButton) findViewById(R.id.radioVegetarian);
-                    RadioButton radioOmnivore = (RadioButton) findViewById(R.id.radioOmnivore);
-                    if (gender.equals("M")) {
-                        radioM.setChecked(true);
-                    } else if (gender.equals("F")) {
-                        radioF.setChecked(true);
-                    } else if (gender.equals("O")) {
-                        radioO.setChecked(true);
-                    }
-                    if (diet.equals("Omniv")) {
-                        radioOmnivore.setChecked(true);
-                    } else if (diet.equals("Vegetarian")) {
-                        radioVegetarian.setChecked(true);
-                    } else if (diet.equals("Vegan")) {
-                        radioVegano.setChecked(true);
-                    }
-                }*/
             }
 
             @Override
@@ -77,28 +81,5 @@ public class RecipeView extends AppCompatActivity {
             }
         };
         FirebaseDatabase.getInstance().getReference("Recipes/" + name).addValueEventListener(valueEventListener);
-
-
-
-
-        /**ingredients += "\t- patata\n";
-        ingredients += "\t2. Quinoa\n";
-        ingredients += "\t1. Salmon\n";
-        ingredients += "\t1. Espinaca\n";
-        ingredients += "\t2. Quinoa\n";
-        ingredients += "\t1. Salmon\n";
-        ingredients += "\t1. Espinaca\n";
-        ingredients += "\t1. patata\n";
-        ingredients += "\t2. Quinoa\n";
-        ingredients += "\t1. Salmon\n";
-        ingredients += "\t1. Espinaca\n";
-        ingredients += "\t2. Quinoa\n";
-        ingredients += "\t1. Salmon\n";
-        ingredients += "\t1. Espinaca\n";*/
-
-        /**description.setText("Lorem ipsum dolor sit amet consectetur adipiscing elit, auctor nulla facilisis sed neque nullam, netus senectus nunc duis tempus per. Nibh elementum feugiat pharetra per dictumst nascetur id viverra, integer consequat mauris venenatis malesuada hac nulla. Metus dignissim vehicula posuere lacinia donec taciti ridiculus eros, per rhoncus viverra tempor pretium arcu integer euismod, lectus id penatibus orci montes nisl eleifend.\n" +
-                "\n" +
-                "Inceptos in curabitur malesuada a sollicitudin per varius tellus nisl, torquent laoreet lectus gravida fusce scelerisque turpis vehicula, nostra parturient porta bibendum accumsan placerat aptent ad. Ultrices penatibus velit viverra metus massa fringilla praesent mi, sodales eu consequat nibh egestas ut mattis hendrerit aliquet, sed pretium ridiculus integer magna gravida potenti. Nisi metus nunc curae vivamus maecenas himenaeos fames feugiat accumsan, auctor parturient libero montes venenatis aptent erat et eros augue, diam viverra nisl faucibus non praesent potenti eget.");*/
-
     }
 }
